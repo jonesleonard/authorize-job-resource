d8c73d92c4282a667a55482250c78e7fe9479045
jonesleonard <leojones4902@gmail.com>
2023-09-17 03:52:29 -0400

    revert check

--- tests/check.sh
+++ tests/check.sh
@@ -6,6 +6,6 @@ set -e -u
 script_location="$(../assets/check)"
 
 test_output() {
-  expected=$(jq -n "{version:{version:\"\"}}")
+  expected='[{ "version": "" }]'
   assert_equals "$expected" "$script_location" "the check script should return a valid output"
 }
