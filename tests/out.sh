ce3c17588d1da3320c3ffa18fb412008bd150763
abram353 <abramley353@gmail.com>
2023-10-12 16:24:32 -0400

    Fix Tests

--- tests/out.sh
+++ tests/out.sh
@@ -51,6 +51,7 @@ test_is_debug_false() {
 # ====================================================
 
 test_validates_gh_user() {
+  set_check_org_membership_response_status_code 0
   test_user='test-user'
   payload='{"params":{},"source":{"access_token":"test-access-token","org":"test-org","users":["'$test_user'"]}}'
   result=$(BUILD_CREATED_BY=$test_user BUILD_JOB_NAME="test-build-job" $script_location "$payload")
@@ -63,6 +64,7 @@ test_validates_gh_user() {
 }
 
 test_invalidates_gh_user() {
+  set_check_org_membership_response_status_code 1
   test_user='test-user'
   payload='{"params":{},"source":{"access_token":"test-access-token","org":"test-org","users":["'$test_user'"]}}'
   result=$(BUILD_CREATED_BY="invalid-user" BUILD_JOB_NAME="test-build-job" $script_location "$payload")
@@ -76,6 +78,7 @@ test_invalidates_gh_user() {
 
 test_validates_gh_user_when_multiple_users_teams() {
   set_check_team_membership_response_status_code 1
+  set_check_org_membership_response_status_code 0
   test_user='test-user'
   payload='{"params":{},"source":{"access_token":"test-access-token","org":"test-org","users":["test-user","jdoe1","jdoe2"],"teams":["test-team-1","test-team-2"]}}'
   result=$(BUILD_CREATED_BY=$test_user BUILD_JOB_NAME="test-build-job" $script_location "$payload")
@@ -93,6 +96,7 @@ test_validates_gh_user_when_multiple_users_teams() {
 
 test_validates_gh_team() {
   set_check_team_membership_response_status_code 0
+  set_check_org_membership_response_status_code 0
   test_user='test-user'
   test_team='test-team'
   payload='{"params":{},"source":{"access_token":"test-access-token","org":"test-org","users":[],"teams":["'$test_team'"]}}'
@@ -107,6 +111,7 @@ test_validates_gh_team() {
 
 test_invalidates_gh_team() {
   set_check_team_membership_response_status_code 1
+  set_check_org_membership_response_status_code 1
   test_user='test-user'
   test_team='test-team'
   payload='{"params":{},"source":{"access_token":"test-access-token","org":"test-org","users":[],"teams":["invalid-team"]}}'
@@ -122,6 +127,7 @@ test_invalidates_gh_team() {
 
 test_validates_gh_team_multiple_users_teams() {
   set_check_team_membership_response_status_code 0
+  set_check_org_membership_response_status_code 0
   test_user='test-user'
   test_team='test-team'
   payload='{"params":{},"source":{"access_token":"test-access-token","org":"test-org","users":["test-user","jdoe1","jdoe2"],"teams":["'$test_team'","test-team-2"]}}'
@@ -140,6 +146,7 @@ test_validates_gh_team_multiple_users_teams() {
 
 test_validates_ghe_team() {
   set_check_team_membership_response_status_code 0
+  set_check_org_membership_response_status_code 0
   test_user='test-user'
   test_team='test-team'
   payload='{"params":{},"source":{"access_token":"test-access-token","ghe_host":"test-ghe_host","org":"test-org","users":[],"teams":["'$test_team'"]}}'
@@ -154,6 +161,7 @@ test_validates_ghe_team() {
 
 test_invalidates_ghe_team() {
   set_check_team_membership_response_status_code 1
+  set_check_org_membership_response_status_code 1
   test_user='test-user'
   test_team='test-team'
   payload='{"params":{},"source":{"access_token":"test-access-token","ghe_host":"test-ghe_host","org":"test-org","users":[],"teams":["'$test_team'"]}}'
